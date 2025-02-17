99882f21e7c3a81a082cf1822859ee6a0d9758b0
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-02-17 06:50:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function setLightMode() {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+}
+
+function setDarkMode() {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+}
